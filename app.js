548187d915c3d2a9deca4a40a059fd0ea91d87d0
Mateo Fernández <qweasdrty13@gmail.com>
2022-06-08 08:05:01 -0300

    a

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
 const express = require("express");
-
+const db = require("./db/index");
 const app = express();
-
-const db = require("db");
-const { status } = require("express/lib/response");
-
 const PORT = 3000;
 
-app.get("/", (req, res) => {
-  res.send("Hello, world!");
-});
 
 
 app.get("/pokemon", async (res, req, next) => { 
   try {
-    pokemons = await db. query("select * from pokemon");
-    return res.status(200).json({ data: pokemons.rows });
-  } 
-    catch (error) {
+    
+    const pokemonResults = []  
+    const pokemons = await db. query(
+      "select p.id pokemon_id, p.name, p.number, p.img p.type, p.type2, p.weigth, p.heigth, p.ability, p.info, p.hp, p.atk, p.def, p.satk, p.sdef, p.spd from  pokemon p join about a on a.id = p.about.id joinn stats s on s.id = p.stats.id")
+      for (let index = 0; index <pokemonResults.length; index++){
+        const element = array[index];
+
+    return res.status(200).json({ data: pokemons.rows})
+  }} catch (error) {
     return next(error)
   }
 })
